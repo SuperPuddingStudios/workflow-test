1dc17c91c5a471c81431541ff29774bfeb9d569e
Super Pudding <superpudding98@gmail.com>
2020-03-31 17:10:59 +0300

    Failing test

--- TestProject/Assets/Tests/TestTest.cs
+++ TestProject/Assets/Tests/TestTest.cs
@@ -12,7 +12,7 @@ namespace Tests
         [Test]
         public void TestTestSimplePasses()
         {
-            Assert.Pass();
+            Assert.Fail();
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
